69c6f50c45e120e9036e605ea31c89b0d1082a0f
wonapack <54420892+wonapack@users.noreply.github.com>
2019-08-23 13:25:28 -0300

    Add files via upload

---
+++ Lista2MatrizPC1/matriz1.c
@@ -0,0 +1,13 @@
+/*
+1) (Desvio Padrão) Crie um programa em C que calcule o desvio padrão de um
+vetor de ponto flutuantes de precisão simples (float) de tamanho 10 fornecido
+pelo usuário. A fórmula do desvio padrão é dada abaixo:
+*/
+int main(void){
+	float desv;
+	float vetor[10];
+	printf("Entre com um vetor de tamanho 10")\n" );
+	scanf("%f",&vetor);
+	for(int i = 0 ; i )
+	printf("O vetor digitado eh: %f",vetor[i]);
+}
